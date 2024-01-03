a2e16fff8532204b0715ea95c93ec9d857afdfdc
Rafi <155255625+Returncode00@users.noreply.github.com>
2024-01-03 14:47:53 +0600

    Add files via upload

---
+++ 01_variables.js
@@ -0,0 +1,22 @@
+let name = "sakif";
+console.log(name);
+console.log(typeof(name));
+
+//let score = undefined;-> NaN
+//let score = "33asd";-> Nan
+//let score = "33";-> 33
+//let score = null ;-> 0
+//let score = true ;-> 1
+let score = ""; //->0
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+accountId = 2021360292;
+name = "Rafi Al Sakif"
+city = "Dhaka"
+emailAddress = "std@ewu.bd"
+
+console.table([accountId , name , city , emailAddress])
---
+++ 02_operations.js
@@ -0,0 +1,9 @@
+let value = 9
+let negvalue = -value
+console.log(negvalue);
+console.log(2**3) //2 to the pow 3
+
+let str1 = "Let's "
+let str2 = "play football"
+
+console.log(str1 + str2)
